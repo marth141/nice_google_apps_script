d49d6b9dbb9f22f79594c2c37c1360aa5d003e70
marth141 <ncasados@live.com>
2020-09-10 10:13:26 -0600

    Removing clasp and url

--- clasp/finance/Controller.ts
+++ clasp/finance/Controller.ts
@@ -3,7 +3,7 @@ class Controller {
   public finance_sheet: FinanceSheet;
 
   constructor() {
-    this.spreadsheet = SpreadsheetApp.openByUrl("https://docs.google.com/spreadsheets/d/1yHw034cKPcdDam94l1WZJxM8QaWXfkZcZ02z9kROTXs/edit");
+    this.spreadsheet = SpreadsheetApp.openByUrl("spreadsheet_url");
     this.finance_sheet = new FinanceSheet(this.spreadsheet);
   }
 }
