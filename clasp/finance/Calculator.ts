3d5bdc2d02f321905014b3ed4c377c058490794c
marth141 <ncasados@live.com>
2020-09-10 12:58:36 -0600

    Revised variable name

--- clasp/finance/Calculator.ts
+++ clasp/finance/Calculator.ts
@@ -2,10 +2,8 @@ class Calculator {
   public total_cost_of_records_to_remove: number;
   public percent_of_removed_to_bill: number;
   constructor(records_to_remove: Array<FinanceRecord>, total_bill: number) {
-    this.total_cost_of_records_to_remove = ((
-      records_to_keep: Array<FinanceRecord>
-    ) =>
-      records_to_keep
+    this.total_cost_of_records_to_remove = ((records: Array<FinanceRecord>) =>
+      records
         .map((record: FinanceRecord) => record.cost)
         .reduce((x, y) => x + y, 0))(records_to_remove);
     this.percent_of_removed_to_bill = ((
