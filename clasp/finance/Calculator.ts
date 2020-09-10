46855eef9c8defe59e2c74e553a760e055082f17
marth141 <ncasados@live.com>
2020-09-10 14:35:44 -0600

    Revised wordage

--- clasp/finance/Calculator.ts
+++ clasp/finance/Calculator.ts
@@ -7,9 +7,9 @@ class Calculator {
         .map((record: FinanceRecord) => record.cost)
         .reduce((x, y) => x + y, 0))(records_to_remove);
     this.percent_of_removed_to_total = ((
-      total_cost_of_records_to_keep: number,
+      total_cost_of_records_to_remove: number,
       total_bill: number
-    ) => total_cost_of_records_to_keep / total_bill)(
+    ) => total_cost_of_records_to_remove / total_bill)(
       this.total_cost_of_records_to_remove,
       total_bill
     );
