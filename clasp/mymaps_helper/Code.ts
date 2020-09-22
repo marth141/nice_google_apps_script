aff671e8f9dfa15f6026e9425ed9970a84ebf677
marth141 <ncasados@live.com>
2020-09-22 16:52:27 -0600

    One last revision

--- clasp/mymaps_helper/Code.ts
+++ clasp/mymaps_helper/Code.ts
@@ -1,9 +1,14 @@
 function myFunction() {
   const { spreadsheet, input_sheet, output_sheet } = new MyMapsHelper();
+  const { fetch_customers } = input_sheet();
+  const { update_output } = output_sheet();
   const { lat_lng_result } = new LatLngCalculator();
-  const update = lat_lng_result(input_sheet().fetch_customers());
 
-  output_sheet().update_output(update);
+  const update = (function get_update() {
+    return lat_lng_result(fetch_customers());
+  })();
+
+  update_output(update);
   spreadsheet().toast(
     "Check Output Sheet and Save/Export Output Sheet as .csv",
     "Completed",
