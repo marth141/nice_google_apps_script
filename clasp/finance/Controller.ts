ccb94567032b421f7e9f7c37121fc706c070130e
marth141 <ncasados@live.com>
2021-01-22 15:26:10 -0700

    Removing some other examples because they're older and a bit yucky

--- clasp/finance/Controller.ts
+++
@@ -1,8 +0,0 @@
-class Controller {
-  private spreadsheet: GoogleAppsScript.Spreadsheet.Spreadsheet;
-  public finance_sheet: FinanceSheet;
-  constructor() {
-    this.spreadsheet = SpreadsheetApp.openByUrl("spreadsheet_url");
-    this.finance_sheet = new FinanceSheet(this.spreadsheet);
-  }
-}
--- clasp/finance/onEdit.ts
+++
@@ -1,8 +0,0 @@
-function onEdit(e: GoogleAppsScript.Events.SheetsOnEdit) {
-  switch (e.range.getColumn()) {
-    case 3:
-      myFunction();
-    default:
-      break;
-  }
-}
